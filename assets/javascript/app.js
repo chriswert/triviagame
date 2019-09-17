1961347784ac423a673db7964aa15cb69e8f2a21
Christopher Werther <wildcatwerther@gmail.com>
2019-09-16 20:47:25 -0700

    html and css pluss intial timer js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,30 @@
-$(document).ready(function(){
+$(document).ready(function() {
+
+   var questions = [
+       {
+       question: "What is the capital of Arizona?",
+       answer: "Phoenix"
+       },
+       {
+        question: "What is the capital of Colorado?",
+        answer: "Denver"
+       }
+    ];
+
+    var timerMeaning = 10;
+
+    var score = 0;
+
+    function countdownTimer(seconds){
+      
+        for (i = seconds; i >= 1; i--){
+                setInterval (countdownTimer, 1000);
+    }
+    };   
+  
+    countdownTimer(10);
 
 
-    
+
+
 });
